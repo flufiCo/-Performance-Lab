22bc83a4bad30812098ee91fc3472a0179b32e07
flufiCo <78958325+flufiCo@users.noreply.github.com>
2024-09-04 22:21:02 +0400

    answer_1.py

--- task1/answer_1.py
+++ task1/answer_1.py
@@ -1,34 +1,24 @@
 import sys
 
-
 def circular_path(n, m):
-    circle = list(range(1, n + 1))
+    array = [i + 1 for i in range(n)]
     path = []
-
-    current_position = 0
-    for _ in range(n // m):
-        current_interval = circle[current_position:current_position + m]
-
-        path.append(current_interval[0])
-        current_position = (current_position + m) % n
-
-    return path
-
+    current_index = 0
+    
+    while True:
+        path.append(array[current_index])
+        current_index = (current_index + m) % n
+        if current_index == 0:
+            break
+    
+    return ''.join(map(str, path))
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Использование: python script.py <n> <m>")
-        sys.exit(1)
+        print("Использование: python script.py n m")
+    else:
+        n = int(sys.argv[1])
+        m = int(sys.argv[2])
+        result = circular_path(n, m)
+        print(result)
 
-    n = int(sys.argv[1])
-    m = int(sys.argv[2])
-
-    result_path = circular_path(n, m)
-    print("Круговой массив:", ''.join(map(str, range(1, n + 1))))
-    print(f"При длине обхода {m} получаем интервалы:", end=" ")
-
-    intervals = [result_path[i:i+m] for i in range(0, len(result_path), m)]
-    for interval in intervals:
-        print("".join(map(str, interval)), end=", ")
-
-    print("\nПолученный путь:", ''.join(map(str, result_path)))
