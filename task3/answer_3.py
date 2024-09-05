6faf54f2eb30be592e64ba07b7438ee2e4cb16c5
flufiCo <78958325+flufiCo@users.noreply.github.com>
2024-09-05 22:21:12 +0400

    Update answer_3.py

--- task3/answer_3.py
+++ task3/answer_3.py
@@ -1,49 +1,39 @@
 import json
 import sys
-import os
 
+def fill_values(tests, values_dict):
+    for test in tests:
+        test_id = test['id']
+        if test_id in values_dict:
+            test['value'] = values_dict[test_id]
+        
+        if 'values' in test:
+            fill_values(test['values'], values_dict)
+    
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <values.json> <tests.json> <report.json>")
+        return
 
-def read_json_file(file_name):
-    file_path = os.path.join(os.path.dirname(__file__), file_name)
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-        return data
-    except FileNotFoundError:
-        print(f"Файл {file_name} не найден.")
-        sys.exit(1)
-    except json.JSONDecodeError:
-        print(f"Ошибка при декодировании JSON в файле {file_name}.")
-        sys.exit(1)
+    values_path = sys.argv[1]
+    tests_path = sys.argv[2]
+    report_path = sys.argv[3]
 
+    with open(values_path, 'r') as f:
+        values = json.load(f)
 
-def update_report_values(report, values):
-    for test_id, test_data in report.items():
-        if "tests" in test_data:
-            update_report_values(test_data["tests"], values)
-        elif test_id in values:
-            test_data["value"] = values[test_id]
+    with open(tests_path, 'r') as f:
+        tests = json.load(f)
 
+    # Создаем словарь для быстрого поиска значений по ID
+    values_dict = {item['id']: item['value'] for item in values}
 
-def write_json_file(data, file_name):
-    file_path = os.path.join(os.path.dirname(__file__), file_name)
-    try:
-        with open(file_path, 'w') as file:
-            json.dump(data, file, indent=2)
-        print(f"Файл {file_name} успешно создан.")
-    except IOError:
-        print(f"Ошибка при записи в файл {file_name}.")
-        sys.exit(1)
+    # Заполняем значения в структуре tests.json
+    fill_values(tests, values_dict)
 
+    # Сохраняем результат в report.json
+    with open(report_path, 'w') as f:
+        json.dump(tests, f, indent=4)
 
 if __name__ == "__main__":
-    values_file = "values.json"
-    tests_file = "tests.json"
-    report_file = "report.json"
-
-    values_data = read_json_file(values_file)
-    tests_data = read_json_file(tests_file)
-
-    update_report_values(tests_data, values_data)
-
-    write_json_file(tests_data, report_file)
+    main()
